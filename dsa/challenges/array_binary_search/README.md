89bf36213fd2224d63657c24f6406327fc2c64ca
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-05-13 19:47:27 -0700

    Code Challenge 03 completed

--- dsa/challenges/array_binary_search/README.md
+++ dsa/challenges/array_binary_search/README.md
@@ -12,5 +12,7 @@ Write a function called `BinarySearch` which takes in 2 parameters: a sorted arr
 ## Approach & Efficiency
 In BinarySearch function I needed a way to compare the search key value to each element of the input array. To do this I utlized a for in range loop with the range equal to the length of the input array. Each iteration array at i was compared to the search key, if a match occured then stop looping and return that index value. If the loop completes its cycle with no match then it hits the return -1 block.
 
+Fixed Approach: I read the article about halfing the search result in order to determine value. To do this I calculated the total length of the array and determined the middle index. Then with some if/else conditionally logic to determine if the key is higher or lower from there cut the array in half and search the half that the is true. Continue this until the number is found.
+
 ## Solution
 ![White Board Image](../../../assets/array_binary_search.png)
--- dsa/challenges/array_binary_search/array_binary_search.py
+++ dsa/challenges/array_binary_search/array_binary_search.py
@@ -1,9 +1,35 @@
+# def BinarySearch(arr, key):
+#     """
+#     BinarySearch takes in two parameters: list of integers, and a search integer.
+#     This function will then search the the list to find a match to the search key.
+#     """
+#     for i in range(len(arr)):
+#         if arr[i] == key:
+#             return i
+#     return -1
+
 def BinarySearch(arr, key):
     """
     BinarySearch takes in two parameters: list of integers, and a search integer.
     This function will then search the the list to find a match to the search key.
     """
-    for i in range(len(arr)):
-        if arr[i] == key:
-            return i
+    if len(arr) == 0:
+        return -1
+
+    if arr[len(arr)-1] < key:
+        return -1
+    else:
+        start_idx = 0
+        end_idx = len(arr) - 1
+
+        while start_idx <= end_idx:
+            middle_idx = (start_idx + end_idx) // 2
+
+            if arr[middle_idx] == key:
+                return middle_idx
+            else:
+                if key < arr[middle_idx]:
+                    end_idx = middle_idx - 1
+                else:
+                    start_idx = middle_idx + 1
     return -1
