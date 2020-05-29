404d115f49837ce5f69895b00f06b050611973d3
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-05-29 10:57:04 -0700

    Challenge 13 Features complete, still need to add approach and Whiteboard image

--- tests/challenges/test_multi_bracket_validation.py
+++ tests/challenges/test_multi_bracket_validation.py
@@ -1,20 +1,53 @@
 import pytest
 from dsa.challenges.multi_bracket_validation.multi_bracket_validation import multi_bracket_validation
 
-@pytest.mark.skip()
 def test_mbv_exists():
     assert multi_bracket_validation('test')
 
-@pytest.mark.skip()
-def test_mbv_three_pairs():
-    string = '()[]{}'
+def test_mbv_one():
+    string = '{}'
     actual = multi_bracket_validation(string)
     expected = True
     assert actual == expected
 
-
-def test_mbv_split():
-    string = '(som)thing[inside]{here}'
+def test_mbv_two():
+    string = '{}(){}'
     actual = multi_bracket_validation(string)
-    expected = [1]
+    expected = True
+    assert actual == expected
+
+def test_mbv_three():
+    string = '()[[Extra Characters]]'
+    actual = multi_bracket_validation(string)
+    expected = True
+    assert actual == expected
+
+def test_mbv_four():
+    string = '(){}[[]]'
+    actual = multi_bracket_validation(string)
+    expected = True
+    assert actual == expected
+
+def test_mbv_five():
+    string = '{}{Code}[Fellows](())'
+    actual = multi_bracket_validation(string)
+    expected = True
+    assert actual == expected
+
+def test_mbv_six():
+    string = '[({}]'
+    actual = multi_bracket_validation(string)
+    expected = False
+    assert actual == expected
+
+def test_mbv_seven():
+    string = '(]('
+    actual = multi_bracket_validation(string)
+    expected = False
+    assert actual == expected
+
+def test_mbv_eight():
+    string = '{(})'
+    actual = multi_bracket_validation(string)
+    expected = False
     assert actual == expected
