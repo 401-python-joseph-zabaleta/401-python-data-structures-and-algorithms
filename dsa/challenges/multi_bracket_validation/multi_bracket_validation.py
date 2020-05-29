d532ca7796afa552f288768723a6ccb598b10087
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-05-29 08:54:31 -0700

    ACP current work, still incomplete

--- dsa/challenges/multi_bracket_validation/multi_bracket_validation.py
+++ dsa/challenges/multi_bracket_validation/multi_bracket_validation.py
@@ -3,8 +3,19 @@ def multi_bracket_validation(string):
     """Takes in a string argument and returns a boolean representing whether or not the brackets within the string are balanced.
     """
     output = False
+    brackets = []
+    test = []
 
-    if string.count('{') == string.count('}') and string.count('(') == string.count(')') and string.count('[') == string.count(')'):
-        output = True
+    if string.count('{') == string.count('}') and string.count('(') == string.count(')') and string.count('[') == string.count(']'):
+
+        for char in string:
+            if char in ['(', ')', '[', ']', '{', '}']:
+                brackets.append(char)
+
+        if brackets[0] in [')', '}', ']']:
+            return False
+
+        for i in range(len(brackets)):
+            test.append(brackets[i])
 
     return output
--- tests/challenges/test_multi_bracket_validation.py
+++ tests/challenges/test_multi_bracket_validation.py
@@ -1,11 +1,20 @@
 import pytest
 from dsa.challenges.multi_bracket_validation.multi_bracket_validation import multi_bracket_validation
 
+@pytest.mark.skip()
 def test_mbv_exists():
     assert multi_bracket_validation('test')
 
+@pytest.mark.skip()
 def test_mbv_three_pairs():
     string = '()[]{}'
     actual = multi_bracket_validation(string)
     expected = True
-    assert actual == True
+    assert actual == expected
+
+
+def test_mbv_split():
+    string = '(som)thing[inside]{here}'
+    actual = multi_bracket_validation(string)
+    expected = [1]
+    assert actual == expected
