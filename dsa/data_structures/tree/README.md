56e2c9146b03c632f3c65b263cbc35592872c7f2
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-05-30 10:13:10 -0700

    ACP

--- dsa/data_structures/tree/README.md
+++ dsa/data_structures/tree/README.md
@@ -4,34 +4,34 @@
 This challenge address the datastruce of trees. We will be working with Binary Trees and Binary Search Trees. We will create methods that will traverse through these trees and do several operations.
 
 ## Features
-- [ ] Create a Node class that has properties for the value stored in the node, the left child node, and the right child node.
-- [ ] Create BinaryTree class:
+- [x] Create a Node class that has properties for the value stored in the node, the left child node, and the right child node.
+- [x] Create BinaryTree class:
     - [ ] Define a method for each of the depth first traversals which returns an array of the values, ordered appropriately.
-        - [ ] `preOrder`
-        - [ ] `inOrder`
-        - [ ] `postOrder`
+        - [x] `pre_order`
+        - [x] `in_order`
+        - [x] `post_order`
 
-- [ ] Create a BinarySearchTree class:
-    - [ ] Define a method named `add` that accepts a value, and adds a new node with that value in the correct location in the binary search tree.
+- [x] Create a BinarySearchTree class:
+    - [x] Define a method named `add` that accepts a value, and adds a new node with that value in the correct location in the binary search tree.
     - [ ] Define a method named `contains` that accepts a value, and returns a boolean indicating whether or not the value is in the tree at least once.
 
 ### Unit Tests
-1. [ ] Can successfully instantiate an empty tree.
-2. [ ] Can successfully instantiate a tree with a single root node.
-3. [ ] Can successfully  add a left child and a right child to a single root node.
-4. [ ] Can successfully return a collection from a preorder traversal.
-5. [ ] Can successfully return a collection from an inorder traversal.
-6. [ ] Can successfully return a collection from a postorder traversal.
+1. [x] Can successfully instantiate an empty tree.
+2. [x] Can successfully instantiate a tree with a single root node.
+3. [x] Can successfully  add a left child and a right child to a single root node.
+4. [x] Can successfully return a collection from a preorder traversal.
+5. [x] Can successfully return a collection from an inorder traversal.
+6. [x] Can successfully return a collection from a postorder traversal.
 
 
 ## Approach & Efficiency
 
 ## API
-`preOrder()`: This is a Depth First traversal method. It prioritizes printing the `root` first, then looks to print `left` if left is not `None`, and lastly looks `right`.
+`pre_order()`: This is a Depth First traversal method. It prioritizes printing the `root` first, then looks to print `left` if left is not `None`, and lastly looks `right`.
 
-`inOrder()`: This is a Depth First traversal method. It prioritizes printing the `left` first, then prints the `root`, and lastly looks to print `right`.
+`in_order()`: This is a Depth First traversal method. It prioritizes printing the `left` first, then prints the `root`, and lastly looks to print `right`.
 
-`postOrder()`: This is a Depth First traversal method. It prioritizes print the `left` first, then looks to print the `right` and lastly prints the `root`.
+`post_order()`: This is a Depth First traversal method. It prioritizes print the `left` first, then looks to print the `right` and lastly prints the `root`.
 
 ## Solution
 ![White Board Image](../../../assets/tree.png)
---
+++ dsa/data_structures/tree/tree.py
@@ -0,0 +1,152 @@
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def pre_order(self):
+        """This is a Depth First traversal method. It prioritizes printing the `root` first, then looks to print `left` if left is not `None`, and lastly looks `right`."""
+        collection = []
+
+        def walk(root):
+            if not root:
+                return
+
+            # <<< root >>>
+            collection.append(root.value)
+
+            # <<< left
+            walk(root.left)
+
+            # right >>>
+            walk(root.right)
+
+        # end
+        walk(self.root)
+
+        return collection
+
+
+    def in_order(self):
+        """This is a Depth First traversal method. It prioritizes printing the `left` first, then prints the `root`, and lastly looks to print `right`."""
+        collection = []
+
+        def walk(root):
+            if not root:
+                return
+
+            # <<< left
+            if root.left == None and root.right == None:
+                collection.append(root.value)
+                return
+            else:
+                walk(root.left)
+
+            # <<< root >>>
+            collection.append(root.value)
+
+            # right >>>
+            if root.left == None and root.right == None:
+                collections.append(root.value)
+                return
+            else:
+                walk(root.right)
+
+        # Invoke
+        walk(self.root)
+
+        return collection
+
+    def post_order(self):
+        """This is a Depth First traversal method. It prioritizes print the `left` first, then looks to print the `right` and lastly prints the `root`."""
+        collection = []
+
+        def walk(root):
+            if not root:
+                return
+
+            # <<< left
+            if root.left == None and root.right == None:
+                collection.append(root.value)
+                return
+            else:
+                walk(root.left)
+
+
+            # right >>>
+            if root.left == None and root.right == None:
+                collections.append(root.value)
+                return
+            else:
+                walk(root.right)
+
+            # <<< root >>>
+            collection.append(root.value)
+
+        # Invoke
+        walk(self.root)
+
+        return collection
+
+
+class BinarySearchTree(BinaryTree):
+    def add(self, value):
+        """This will add a new element to the tree, based on a tradtional binary search tree condtional. If value is smaller than the root it will be added to the left, else add to the right."""
+
+        node = Node(value)
+
+        # Handles empty tree
+        if not self.root:
+            self.root = node
+            return
+
+        def walk(root, new_node):
+
+            if not root:
+                return
+
+            # <<< left
+            if new_node.value < root.value:
+
+                if not root.left:
+                    root.left = new_node
+                else:
+                    walk(root.left, new_node)
+
+            # right >>>
+            else:
+                if not root.right:
+                    root.right = new_node
+                else:
+                    walk(root.right, new_node)
+
+        walk(self.root, node)
+
+    def contains(self, value):
+        """This searches the tree in order to verify that a given value exists in the tree. Returns a boolean value."""
+        # Remember Pre_order returns a collection list
+        if value in self.pre_order():
+            return True
+        else:
+            return False
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
