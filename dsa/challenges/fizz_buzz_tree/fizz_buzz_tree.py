629084594946f920b54a1b98c06afdeb6613db7d
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-06-01 18:23:11 -0700

    Challenge 16 Completed

--- dsa/challenges/fizz_buzz_tree/fizz_buzz_tree.py
+++ dsa/challenges/fizz_buzz_tree/fizz_buzz_tree.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 class Node:
     def __init__(self, value, left=None, right=None):
         self.value = value
@@ -10,27 +12,31 @@ class BinaryTree:
         self.root = None
 
     def add(self, value):
+        """ This is the default add method that will add nodes to a tree with Breadth First logic
+        """
         new_node = Node(value)
         breadth = Queue()
         breadth.enqueue(self.root)
+
+        if not self.root:
+            self.root = new_node
+            return
 
         while not breadth.is_empty():
             front = breadth.dequeue()
 
             if not front.left:
                 front.left = new_node
+                return
             elif not front.right:
                 front.right = new_node
-            else:
-                pass
+                return
 
             if front.left:
                 breadth.enqueue(front.left)
 
             if front.right:
                 breadth.enqueue(front.right)
-
-
 
     def pre_order(self):
         """This is a Depth First traversal method. It prioritizes printing the `root` first, then looks to print `left` if left is not `None`, and lastly looks `right`."""
@@ -117,6 +123,8 @@ class BinaryTree:
         return collection
 
     def breadth_first(self):
+        """ This is a Breadth first traversal method that iterates through the tree by going through each level of the tree node by node.
+        """
         collection = []
         breadth = Queue()
         breadth.enqueue(self.root)
@@ -133,80 +141,49 @@ class BinaryTree:
 
         return collection
 
-class BinarySearchTree(BinaryTree):
-    def add(self, value):
-        """This will add a new element to the tree, based on a tradtional binary search tree condtional. If value is smaller than the root it will be added to the left, else add to the right."""
-
-        node = Node(value)
-
-        # Handles empty tree
-        if not self.root:
-            self.root = node
-            return
-
-        def walk(root, new_node):
-
-            if not root:
-                return
-
-            # <<< left
-            if new_node.value < root.value:
-
-                if not root.left:
-                    root.left = new_node
-                else:
-                    walk(root.left, new_node)
-
-            # right >>>
-            else:
-                if not root.right:
-                    root.right = new_node
-                else:
-                    walk(root.right, new_node)
-
-        walk(self.root, node)
-
-    def contains(self, value):
-        """This searches the tree in order to verify that a given value exists in the tree. Returns a boolean value."""
-        # Remember Pre_order returns a collection list
-        if value in self.pre_order():
-            return True
-        else:
-            return False
-
 class Queue:
     def __init__(self):
-        self.front = None
-        self.rear = None
+        self.storage = deque()
 
-    def enqueue(self, item):
+    def enqueue(self, value):
         """Takes any value as an argument and adds a new node with that value to the back of the queue with an O(1) Time Performance."""
-        if self.rear == None:
-            self.front = Node(item)
-            self.rear = self.front
-        else:
-            self.rear.next = Node(item)
-            self.rear = self.rear.next
+        self.storage.appendleft(value)
 
     def dequeue(self):
         """Takes no arguments, remove the node from the front of the queue, and returns the node's value."""
-        if not self.front:
-            raise AttributeError("Can't dequeue from an empty queue")
-
-        removed = self.front
-        self.front = self.front.next
-        return removed
+        return self.storage.pop()
 
     def peek(self):
         """Takes no arguments and returns the value of the node located in the front of the queue, without removing it from the queue."""
-        if not self.front:
-            raise AttributeError("Can't peek from an empty queue")
-        return self.front.value
+        return self.storage[-1]
 
     def is_empty(self):
         """Takes no arguments and returns a boolean indicating whether or not the queue is empty."""
-        return not bool(self.front)
-
+        return len(self.storage) == 0
 
 def fizz_buzz_tree(tree):
-    pass
+    """Takes in a tree as a single argument. Changes values throughout the tree based on Fizzbuzz logic, and returns a new tree in the same order and structure.
+    """
+    collection = tree.breadth_first()
+    new_collection = []
+
+    for i in collection:
+        if i % 3 == 0 and i % 5 == 0:
+            i = 'FizzBuzz'
+            new_collection.append(i)
+        elif i % 3 == 0:
+            i = 'Fizz'
+            new_collection.append(i)
+        elif i % 5 == 0:
+            i = 'Buzz'
+            new_collection.append(i)
+        else:
+            i = str(i)
+            new_collection.append(i)
+
+    new_tree = BinaryTree()
+
+    for i in new_collection:
+        new_tree.add(i)
+
+    return new_tree
