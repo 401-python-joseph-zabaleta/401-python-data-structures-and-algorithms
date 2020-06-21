6ec6366733de928d07ee6d86bccce17c683c4c15
joseph-zabaleta <joseph.l.zabaleta@gmail.com>
2020-06-21 11:44:45 -0700

    added code challenge 30

--- dsa/data_structures/hashtable/hashtable.py
+++ dsa/data_structures/hashtable/hashtable.py
@@ -22,7 +22,7 @@ class LinkedList:
         collection = []
         current = self.head
         while current:
-            collection.append(current.data)
+            collection.append(current.data[0])
             current = current.next
         return collection
 
@@ -36,32 +36,53 @@ class Hashmap:
         self.map = [None] * self.size
 
     def add(self, key, value):
+        """Add is reponsible for adding data to the hashmap datas structure
+        """
         hashed_key = self.hash(key)
 
         if not self.map[hashed_key]:
-            self.map[hash_key] = LinkedList()
+            self.map[hashed_key] = LinkedList()
 
         self.map[hashed_key].add([key, value])
 
-    def get(self):
-        pass
+    def get(self, key):
+        """Get is responsible for taking in a key argument and returning the value for that key in the hashmap
+        """
+        index = self.hash(key)
 
-    def contains(self):
-        pass
+        if self.map[index]:
+            ll = self.map[index]
+
+            while ll.head:
+                if ll.head.data[0] == key:
+                    return ll.head.data[1]
+                else:
+                    ll.head = ll.head.next
+        else:
+            return None
+
+    def contains(self, key):
+        """Contains is reponsible for returning a bool for wether or not the provided key is within the data structure
+        """
+        index = self.hash(key)
+
+        if self.map[index]:
+            collection = self.map[index].display()
+            if key in collection:
+                return True
+            else:
+                pass
+        return False
 
     def hash(self, key):
         """
-        1. split the key
-        2. conver to ascii and add together
-        3. multiple by prime
-        4. % by self.size
-        5. return
+        Hash is responsible for splitting they key, converting to ascii values, adding them together, multiply it by any prime number, then modulous by the size of the hashmap to return a valid index value within the hashmap to store that key.
         """
         total = 0
         for char in key:
             total += ord(char)
 
-        total += 19
+        total *= 19
 
         hashed_key = total % self.size
 
